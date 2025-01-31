c072939cab82d626aff7afb6ac98025aa8f7f3bf
Eliana Dheys B. Ojeda <164065005+liandh@users.noreply.github.com>
2025-01-31 16:56:47 +0800

    added header block

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,30 @@
+/*  
+ *    =============================================================================
+ *            App.test.js 
+ *                    - Contains unit tests for the App component.
+ *
+ *            Purpose
+ *                    - This file is used to test if the "learn react" link is rendered 
+ *                      correctly within the App component using React Testing Library.
+ * 
+ *            Key Components:
+ *                     - Test: Renders the App component and checks for the presence of
+ *                       specific text within the component.
+ * 
+ *            Algorithms:
+ *                     - React Testing Library render function to render components in 
+ *                       test environments.
+ * 
+ *            Control Flow:
+ *                     - Simple control flow to render and test the component.
+ * 
+ *            Author: Alleina Abad
+ *            Date: November 16, 2024
+ *            Revised: January 31, 2025
+ *    =============================================================================
+ */
+
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
